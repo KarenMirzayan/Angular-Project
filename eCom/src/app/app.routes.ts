a2121b9313aa25a4a942d10c4bc04064ee5edf57
Karen Mirzayan <k_mirzayan@kbtu.kz>
2024-10-18 15:45:21 +0500

    user and css

--- eCom/src/app/app.routes.ts
+++ eCom/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import { CartComponent } from './cart/cart.component';
+import {LoginComponent} from "./login/login.component";
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'cart', component: CartComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' } 
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
--- eCom/src/app/header/header.component.ts
+++ eCom/src/app/header/header.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import {NgClass, NgOptimizedImage} from "@angular/common";
 import {FormsModule} from "@angular/forms";
+import {RouterLink} from "@angular/router";
+import {faBarsStaggered, faMagnifyingGlass, faShoppingCart} from "@fortawesome/free-solid-svg-icons";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faUserCircle} from "@fortawesome/free-regular-svg-icons";
 
 @Component({
   selector: 'app-header',
@@ -8,13 +12,19 @@ import {FormsModule} from "@angular/forms";
   imports: [
     NgClass,
     NgOptimizedImage,
-    FormsModule
+    FormsModule,
+    RouterLink,
+    FaIconComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   isOpen = false;
+  faCart = faShoppingCart;
+  faUser = faUserCircle
+  faBars = faBarsStaggered;
+  faSearch = faMagnifyingGlass
 
   toggleSidebar() {
     this.isOpen = !this.isOpen;
