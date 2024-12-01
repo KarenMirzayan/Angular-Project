f61413f1bbfb1e5d939c4fc1e2805387415306d3
Karen Mirzayan <k_mirzayan@kbtu.kz>
2024-12-01 16:45:50 +0500

    header

--- eCom/src/app/header/header.component.ts
+++ eCom/src/app/header/header.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import {NgClass, NgOptimizedImage} from "@angular/common";
+import {Component, HostListener} from '@angular/core';
+import {CommonModule, NgClass, NgOptimizedImage} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {RouterLink} from "@angular/router";
 import {faBarsStaggered, faMagnifyingGlass, faShoppingCart} from "@fortawesome/free-solid-svg-icons";
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {faUserCircle} from "@fortawesome/free-regular-svg-icons";
+import {Category} from "../../categories.model";
 
 @Component({
   selector: 'app-header',
@@ -14,7 +15,8 @@ import {faUserCircle} from "@fortawesome/free-regular-svg-icons";
     NgOptimizedImage,
     FormsModule,
     RouterLink,
-    FaIconComponent
+    FaIconComponent,
+    CommonModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
@@ -24,15 +26,63 @@ export class HeaderComponent {
   faCart = faShoppingCart;
   faUser = faUserCircle
   faBars = faBarsStaggered;
-  faSearch = faMagnifyingGlass
+  faSearch = faMagnifyingGlass;
 
-  toggleSidebar() {
-    this.isOpen = !this.isOpen;
-  }
+  isDropdownOpen=false;
+  categories:Category[] = [
+    {
+      id: 1,
+      name: 'Food'
+    },
+    {
+      id: 2,
+      name: 'Clothes'
+    }
+  ]
 
   isSearching = false;
 
+  isHidden= false;
+  private lastScrollTop=0;
+
+  @HostListener('window:scroll', [])
+    onWindowScroll() {
+    const currentScroll = window.scrollY || document.documentElement.scrollTop;
+
+    if (currentScroll <= 66) {
+      // Always show the header when near the top
+      this.isHidden = false;
+      this.lastScrollTop = currentScroll; // Reset last scroll position
+      return;
+    }
+
+    if (Math.abs(currentScroll - this.lastScrollTop) > 30) {
+      if (currentScroll > this.lastScrollTop) {
+        // Scrolling down
+        this.isHidden = true;
+      } else {
+        // Scrolling up
+        this.isHidden = false;
+      }
+      this.lastScrollTop = currentScroll; // Update last scroll position
+    }
+  }
+
+  toggleSidebar() {
+    this.isOpen = !this.isOpen;
+    if (this.isOpen) {
+      document.body.classList.add('noscroll')
+    } else {
+      document.body.classList.remove('noscroll');
+    }
+  }
+
   openSearch() {
     this.isSearching = !this.isSearching;
+  }
+
+  toggleDropdown(event: Event) {
+    event.preventDefault()
+    this.isDropdownOpen = !this.isDropdownOpen;
   }
 }
