db855fb8c2bcb8525a58123fae912918638a523e
Alina Abdullina <fianit8@gmail.com>
2024-12-05 01:37:30 +0500

    adjust styles

--- e-com/src/app/product-card/product-card.component.ts
+++ e-com/src/app/product-card/product-card.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import {StarsComponent} from "../stars/stars.component";
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-product-card',
@@ -10,6 +11,18 @@ import {StarsComponent} from "../stars/stars.component";
   ],
   styleUrls: ['./product-card.component.css']
 })
-export class ProductCardComponent {
+export class ProductCardComponent implements OnInit {
   @Input() product: any; // Accept product data as input
+  categoryName: string | null = null; // To store the category name
+
+  constructor(private categoryService: CategoryService) {}
+
+  ngOnInit(): void {
+    if (this.product?.categoryId) {
+      this.categoryService.getCategoryById(this.product.categoryId).subscribe((category) => {
+        this.categoryName = category ? category.name : 'Unknown Category';
+      });
+    }
+  }
+
 }
--- e-com/src/app/services/category.service.ts
+++ e-com/src/app/services/category.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Firestore, collection, collectionData, doc, getDoc } from '@angular/fire/firestore';
+import { from, Observable } from 'rxjs';
 
 export interface Category {
   id?: string; // Firestore auto-generates document IDs
@@ -11,11 +11,18 @@ export interface Category {
   providedIn: 'root',
 })
 export class CategoryService {
+  private collectionName = 'categories';
+
   constructor(private firestore: Firestore) {}
 
   // Method to fetch all categories
   getCategories(): Observable<Category[]> {
     const categoryCollection = collection(this.firestore, 'categories'); // Reference to the 'category' collection
     return collectionData(categoryCollection, { idField: 'id' }) as Observable<Category[]>;
+  }
+
+  getCategoryById(categoryId: string): Observable<any> {
+    const categoryDocRef = doc(this.firestore, `${this.collectionName}/${categoryId}`);
+    return from(getDoc(categoryDocRef).then((docSnap) => (docSnap.exists() ? docSnap.data() : null)));
   }
 }
